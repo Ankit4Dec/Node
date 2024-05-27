31ab4b7f1f84d816fbfb1cb9a96f2d85e2a607cd
Ankit <ankit27866@gmail.com>
2024-05-27 22:18:13 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
 
 // Define the MongoDB connection URL
-const mongoURL = "mongodb://localhost:27017/hotels";
+// const mongoURL = "mongodb://localhost:27017/hotels";  // Replace 'mydatabase' with your database name
+// const mongoURL = "mongodb+srv://helloworld:Htipl1234@cluster0.7pbsawp.mongodb.net/"
+// const mongoURL = process.env.DB_URL_LOCAL;
+const mongoURL = process.env.DB_URL;
 
 // Set up MongoDB connection
 // mongoose.connect(mongoURL);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //req.body
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -24,7 +26,8 @@ app.use("/person", personRoutes);
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/menu',menuRoutes)
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log("Port 3000 running in the browser");
 });
 
